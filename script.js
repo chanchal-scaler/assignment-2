1b7365326bce78ed05a8cc923b95b9f2bc7a2cd0
Chanchal Mishra <chanchal.mishra@scaler.com>
2022-07-20 13:29:40 +0530

    fixing dropdown and moving styles control to css

--- script.js
+++ script.js
@@ -5,84 +5,60 @@ const inputActiveColor = "#0080ff",
   errColor = "#e22d4c",
   labelColor = "#61738e";
 
-const inputBoxes = document.querySelectorAll("[data-class]"),
-  professionProfessional = document.querySelector(
-    "[data-class='profession-professional']"
-  ),
-  professionStudent = document.querySelector(
-    "[data-class='profession-student']"
-  ),
-  avatarImage = document.querySelector("[data-img='avatar-img']"),
-  submitButton = document.querySelector("[data-btn='submit']");
+const inputBoxes = document.querySelectorAll("[data-class]");
+const professionProfessional = document.querySelector(
+  "[data-class='profession-professional']"
+);
+const professionStudent = document.querySelector(
+  "[data-class='profession-student']"
+);
+const avatarImage = document.querySelector("[data-img='avatar-img']");
+const submitButton = document.querySelector("[data-btn='submit']");
 
-let valid = true,
-  professionSelected = false;
+let valid = true;
+let professionSelected = false;
 
 const validate = (inputBox) => {
   const input = inputBox.querySelector("input");
-  const label = inputBox.querySelector("label");
   const boxName = inputBox.getAttribute("data-class");
   if (!input?.value?.match(validator[boxName])) {
-    if (document.querySelector(`[data-err='${boxName}']`)) {
+    if (boxName != "profession-student" && boxName != "profession-professional")
       valid = false;
-      document.querySelector(`[data-err='${boxName}']`).style.display =
-        "inline";
-      input.style.borderColor = errColor;
-      label.style.color = errColor;
-    }
-  } else {
-    if (document.querySelector(`[data-err='${boxName}']`)) {
-      document.querySelector(`[data-err='${boxName}']`).style.display = "none";
-      const label = inputBox.querySelector("label");
-      input.style.borderColor = inputActiveColor;
-      label.style.color = inputActiveColor;
-    }
-  }
+    inputBox.setAttribute("data-status", "err");
+  } else inputBox.setAttribute("data-status", "active");
 };
 
 const focusOut = (inputBox) => {
   const input = inputBox.querySelector("input");
   if (input.value?.length) validate(inputBox);
-  else {
-    const boxName = inputBox.getAttribute("data-class");
-    document.querySelector(`[data-err='${boxName}']`).style.display = "none";
-    const input = inputBox.querySelector("input");
-    const label = inputBox.querySelector("label");
-    input.style.borderColor = borderColor;
-    label.style.color = labelColor;
-    label.style.top = "2.5rem";
-    label.style.fontSize = "1rem";
-  }
+  else inputBox.setAttribute("data-status", "");
 };
 
 const setProfession = (active, inactive) => {
   professionSelected = true;
-  document.querySelector("[data-err='profession']").style.display = "none";
-  active.style.borderColor = inputActiveColor;
-  active.style.backgroundColor = professionBgColor;
-  inactive.style.backgroundColor = white;
-  inactive.style.borderColor = borderColor;
+  active.setAttribute("data-status", "active");
+  inactive.setAttribute("data-status", "");
 };
 
 const activateInputBox = (inputBox) => {
   if (inputBox.getAttribute("data-class") === "profession-student") {
     setProfession(inputBox, professionProfessional);
-    avatarImage.setAttribute("src", avatarImage.getAttribute("data-student"));
+    avatarImage.setAttribute(
+      "src",
+      avatarImage.getAttribute("data-student-img")
+    );
   } else if (
     inputBox.getAttribute("data-class") === "profession-professional"
   ) {
     setProfession(inputBox, professionStudent);
     avatarImage.setAttribute(
       "src",
-      avatarImage.getAttribute("data-professional")
+      avatarImage.getAttribute("data-professional-img")
     );
   } else {
     const input = inputBox.querySelector("input");
-    const label = inputBox.querySelector("label");
     input.focus();
-    input.style.borderColor = inputActiveColor;
-    if (label) label.style.top = ".6rem";
-    if (label) label.style.fontSize = ".8rem";
+    inputBox.setAttribute("data-status", "active");
   }
 };
 
@@ -91,7 +67,7 @@ for (const inputBox of inputBoxes) {
 
   inputBox
     .querySelector("input")
-    ?.addEventListener("focusout", (event) => focusOut(inputBox));
+    ?.addEventListener("focusout", () => focusOut(inputBox));
 }
 
 // Resetting the form
@@ -100,17 +76,8 @@ const reset = () => {
   professionSelected = false;
   for (const inputBox of inputBoxes) {
     const input = inputBox.querySelector("input");
-    const label = inputBox.querySelector("label");
-    if (input) {
-      input.value = "";
-      input.style.borderColor = borderColor;
-      label.style.color = labelColor;
-      label.style.top = "2.5rem";
-      label.style.fontSize = "1rem";
-    } else {
-      inputBox.style.borderColor = borderColor;
-      inputBox.style.backgroundColor = white;
-    }
+    if (input) input.value = "";
+    inputBox.setAttribute("data-status", "");
   }
   avatarImage.setAttribute(
     "src",
@@ -124,19 +91,22 @@ const reset = () => {
 // Handle Submit
 
 submitButton.addEventListener("click", (event) => {
-  valid = true;
   event.preventDefault();
-  for (const inputBox of inputBoxes) {
-    validate(inputBox);
-  }
-  if (!professionSelected) {
-    document.querySelector("[data-err='profession']").style.display = "inline";
-  }
+  valid = true;
+  for (const inputBox of inputBoxes) validate(inputBox);
+  if (!professionSelected)
+    document
+      .querySelector("[data-err='profession']")
+      .setAttribute("data-status", "err");
   if (valid && professionSelected) {
     reset();
-    document.querySelector('[data-alert="submit"]').style.opacity = 1;
+    document
+      .querySelector('[data-alert="submit"]')
+      .setAttribute("data-status", "active");
     setTimeout(() => {
-      document.querySelector('[data-alert="submit"]').style.opacity = 0;
+      document
+        .querySelector('[data-alert="submit"]')
+        .setAttribute("data-status", "");
     }, 2000);
   }
 });
@@ -145,9 +115,14 @@ submitButton.addEventListener("click", (event) => {
 
 let phones = Object.values(phoneCodes);
 
-let searchField = document.querySelector("[data-class='country-code']"),
-  dropdown = document.querySelector('[data-codes="country"]'),
-  dropdownCaret = document.querySelector('span[data-caret="phone"]');
+let searchField = document.querySelector("[data-class='country-code']");
+let dropdown = document.querySelector('[data-codes="country"]');
+let dropdownCaret = document.querySelector('[data-caret="phone"]');
+
+const setCountryCode = (event) => {
+  searchField.querySelector("input").value = event.target.innerHTML;
+  dropdown.setAttribute("data-status", "");
+};
 
 const search = (event) => {
   const filteredPhones = phones.filter((phone) =>
@@ -159,29 +134,32 @@ const search = (event) => {
     .join("");
 
   let codes = dropdown.querySelectorAll("span");
-  for (let i = 0; i < codes.length; i++)
-    codes[i].addEventListener("click", setCountryCode);
+  for (let code of codes) code.addEventListener("click", setCountryCode);
 
-  dropdown.style.display = "flex";
+  dropdown.setAttribute("data-status", "active");
 };
 
 searchField.addEventListener("keyup", search);
 
 searchField.addEventListener("focusout", (event) => {
-  if (!phones.includes(event.target.value)) {
+  if (!phones.includes(event.target.value))
     searchField.querySelector("input").value = "+91";
-  } else dropdown.style.display = "none";
-  searchField.querySelector("input").style.borderColor = borderColor;
+  else dropdown.setAttribute("data-status", "");
+  searchField.querySelector("input").setAttribute("data-status", "inactive");
 });
 
 searchField.querySelector("input").addEventListener("focus", (event) => {
+  event.target.setAttribute("data-status", "active");
   if (event.target.value === "+91") {
     event.target.value = "+";
     search(event);
   }
 });
 
-const setCountryCode = (event) => {
-  searchField.querySelector("input").value = event.target.innerHTML;
-  dropdown.style.display = "none";
-};
+dropdownCaret.addEventListener("click", () => {
+  if (dropdown.getAttribute("data-status") === "active")
+    dropdown.setAttribute("data-status", "");
+  else if (searchField.querySelector("input").value === "+91")
+    searchField.querySelector("input").focus();
+  else dropdown.setAttribute("data-status", "active");
+});
